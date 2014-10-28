cda1d696254ab9d666e0b1c40fc1a042a162030f
Russell King <rmk+kernel@arm.linux.org.uk>
2014-10-28 15:27:07 +0000

    ARM: enable bpf syscall
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/uapi/asm/unistd.h
+++ arch/arm/include/uapi/asm/unistd.h
@@ -416,6 +416,8 @@
  * This may need to be greater than __NR_last_syscall+1 in order to
  * account for the padding in the syscall table
  */
+#define __NR_memfd_create               (__NR_SYSCALL_BASE+385)
+#define __NR_bpf                        (__NR_SYSCALL_BASE+386)
 
 /*
  * The following SWIs are ARM private.
