0ceb0686370d6480d5564d7b0bc20c4c25215322
Tejun Heo <tj@kernel.org>
2015-12-07 17:38:51 -0500

    netprio_cgroup: limit the maximum css->id to USHRT_MAX
    netprio builds per-netdev contiguous priomap array which is indexed by
    css->id.  The array is allocated using kzalloc() effectively limiting
    the maximum ID supported to some thousand range.  This patch caps the
    maximum supported css->id to USHRT_MAX which should be way above what
    is actually useable.
    
    This allows reducing sock->sk_cgrp_prioidx to u16 from u32.  The freed
    up part will be used to overload the cgroup related fields.
    sock->sk_cgrp_prioidx's position is swapped with sk_mark so that the
    two cgroup related fields are adjacent.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Acked-by: Daniel Wagner <daniel.wagner@bmw-carit.de>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    CC: Neil Horman <nhorman@tuxdriver.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>
    Change-Id: Ib5159c48bfc6d4b703ebff5f617f2fef3651c780

--- include/net/sock.h
+++ include/net/sock.h
@@ -378,9 +378,7 @@ struct sock {
     unsigned short        sk_ack_backlog;
     unsigned short        sk_max_ack_backlog;
     __u32            sk_priority;
-#if IS_ENABLED(CONFIG_NETPRIO_CGROUP)
-    __u32            sk_cgrp_prioidx;
-#endif
+    __u32                   sk_mark;
     struct pid        *sk_peer_pid;
     const struct cred    *sk_peer_cred;
     long            sk_rcvtimeo;
@@ -408,6 +406,9 @@ struct sock {
                           struct sk_buff *skb);
     void                    (*sk_destruct)(struct sock *sk);
     struct          sock_reuseport __rcu	*sk_reuseport_cb;
+#if IS_ENABLED(CONFIG_CGROUP_NET_PRIO)
+       u16                     sk_cgrp_prioidx;
+#endif
 };
 
 /*
