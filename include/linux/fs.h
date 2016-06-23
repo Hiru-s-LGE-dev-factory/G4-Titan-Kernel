2fc041789bd307977c45908a30bca628f4841146
Daniel Micay <danielmicay@gmail.com>
2016-06-23 17:05:09 -0400

    port of grsecurity's DEVICE_SIDECHANNEL feature
    Signed-off-by: Laster K. (lazerl0rd) <officiallazerl0rd@gmail.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2796,4 +2796,10 @@ extern bool path_noexec(const struct path *path);
 
 extern bool path_noexec(const struct path *path);
 
+static inline bool is_sidechannel_device(const struct inode *inode)
+{
+	umode_t mode = inode->i_mode;
+	return ((S_ISCHR(mode) || S_ISBLK(mode)) && (mode & (S_IROTH | S_IWOTH)));
+}
+
 #endif /* _LINUX_FS_H */
