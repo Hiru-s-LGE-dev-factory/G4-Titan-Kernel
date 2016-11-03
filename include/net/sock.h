2483b82e5c8678481792c6c8b7a3cf33c0d22667
Lorenzo Colitti <lorenzo@google.com>
2016-11-04 02:23:41 +0900

    net: core: Add a UID field to struct sock.
    Protocol sockets (struct sock) don't have UIDs, but most of the
    time, they map 1:1 to userspace sockets (struct socket) which do.
    
    Various operations such as the iptables xt_owner match need
    access to the "UID of a socket", and do so by following the
    backpointer to the struct socket. This involves taking
    sk_callback_lock and doesn't work when there is no socket
    because userspace has already called close().
    
    Simplify this by adding a sk_uid field to struct sock whose value
    matches the UID of the corresponding struct socket. The semantics
    are as follows:
    
    1. Whenever sk_socket is non-null: sk_uid is the same as the UID
       in sk_socket, i.e., matches the return value of sock_i_uid.
       Specifically, the UID is set when userspace calls socket(),
       fchown(), or accept().
    2. When sk_socket is NULL, sk_uid is defined as follows:
       - For a socket that no longer has a sk_socket because
         userspace has called close(): the previous UID.
       - For a cloned socket (e.g., an incoming connection that is
         established but on which userspace has not yet called
         accept): the UID of the socket it was cloned from.
       - For a socket that has never had an sk_socket: UID 0 inside
         the user namespace corresponding to the network namespace
         the socket belongs to.
    
    Kernel sockets created by sock_create_kern are a special case
    of #1 and sk_uid is the user that created them. For kernel
    sockets created at network namespace creation time, such as the
    per-processor ICMP and TCP sockets, this is the user that created
    the network namespace.
    
    [Backport of net-next 86741ec25462e4c8cdce6df2f41ead05568c7d5e]
    
    Bug: 16355602
    Change-Id: I73e1a57dfeedf672f4c2dfc9ce6867838b55974b
    Signed-off-by: Lorenzo Colitti <lorenzo@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/sock.h
+++ include/net/sock.h
@@ -408,6 +408,7 @@ struct sock {
                           struct sk_buff *skb);
     void                    (*sk_destruct)(struct sock *sk);
     struct          sock_reuseport __rcu	*sk_reuseport_cb;
+	kuid_t			sk_uid;
 };
 
 /*
@@ -1715,10 +1716,16 @@ static inline void sock_graft(struct sock *sk, struct socket *parent)
     sk_set_socket(sk, parent);
     security_sock_graft(sk, parent);
     write_unlock_bh(&sk->sk_callback_lock);
+    sk->sk_uid = SOCK_INODE(parent)->i_uid;
 }
 
 extern kuid_t sock_i_uid(struct sock *sk);
 extern unsigned long sock_i_ino(struct sock *sk);
+
+static inline kuid_t sock_net_uid(const struct net *net, const struct sock *sk)
+{
+	return sk ? sk->sk_uid : make_kuid(net->user_ns, 0);
+}
 
 static inline struct dst_entry *
 __sk_dst_get(struct sock *sk)
